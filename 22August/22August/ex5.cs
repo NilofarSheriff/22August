ee0da89efabb329470b126829abd69de68f2e8de
Nilofar <nilofarsheriff@gmail.com>
2022-08-22 15:02:36 +0530

    Ex5 uncompleted

---
+++ 22August/22August/ex5.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22August
+{
+    internal class ex5
+    {
+        static void Main(string[] args)
+        {
+            int[][] jagged_arr1 = new int[4][];
+        }
+    }
+}
