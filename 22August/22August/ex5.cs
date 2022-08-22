5e73c67f9adba4a2d22d950e24a70afb70cdb0bc
Nilofar <nilofarsheriff@gmail.com>
2022-08-22 22:05:08 +0530

    Ex.5 Completed

--- 22August/22August/ex5.cs
+++ 22August/22August/ex5.cs
@@ -10,7 +10,25 @@ namespace _22August
     {
         static void Main(string[] args)
         {
-            int[][] jagged_arr1 = new int[4][];
+            string[][] classsection = new string[4][];
+            classsection[0] = new string[] { "Abirami", "Bala", "chevireddy" };
+            classsection[1] = new string[] { "Aham", "Brindha", "Israth" };
+            classsection[2] = new string[] { "Varshini", "Nilofar", "Sathya","Abinaya" };
+            classsection[3] = new string[] { "Janani", "Shobiya", "Preetha", "Shalini" };
+
+            for(int i = 0; i < classsection.Length; i++)
+            {
+                Console.WriteLine("Row [{0}]:",i);
+                for(int j = 0; j < classsection[i].Length; j++)
+                {
+                    Console.Write(classsection[i][j]+" ");
+                    
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
         }
     }
+
 }
